9117935304ce396316264d6fd46c1f119a9f0e75
smpldvd <david.n.luu@gmail.com>
2018-05-31 01:25:19 -0500

    Got everything working but still working on style.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,78 +5,78 @@ $(document).ready(function () {
     var bank = [
         {
             q: "In database programming, SQL is an acronym for what?",
-            ch: ["Some Questionable Language", "Structured Query Language", "Stand Queue Later", "Silly Quick Laugh"],
-            co: "Structured Query Language",
-            img: "assets/images/computers.gif"
+            cho: ["Some Questionable Language", "Structured Query Language", "Stand Queue Later", "Silly Quick Laugh"],
+            cor: 1,
+            img: "assets/images/computer.gif"
         },
         {
             q: "What was the first console video game that allowed the game to be saved?",
-            ch: ["Mario Bros.", "Mega Man", "Tetris", "The Legend of Zelda"],
-            co: "The Legend of Zelda",
+            cho: ["Mario Bros.", "Mega Man", "Tetris", "The Legend of Zelda"],
+            cor: 3,
             img: "assets/images/zelda.gif"
         },
         {
             q: "On the hit show Seinfeld what was Kramer’s first name?",
-            ch: ["Cosmos", "Kramer", "Jerry", "George"],
-            co: "Cosmos",
+            cho: ["Cosmos", "Kramer", "Jerry", "George"],
+            cor: 0,
             img: "assets/images/kramer.gif"
         },
         {
             q: "The first movie of the Fast and Furious franchise was released in what year?",
-            ch: ["2000", "2001", "2004", "2007"],
-            co: "2001",
+            cho: ["2000", "2001", "2004", "2007"],
+            cor: 1,
             img: "assets/images/ff.gif"
         },
         {
             q: "Which is not one of the four houses at Hogwarts School of Witchcraft and Wizardry?",
-            ch: ["Gryffindor", "Dormchester", "Ravenclaw", "Hufflepuff"],
-            co: "Dormchester",
+            cho: ["Gryffindor", "Dormchester", "Ravenclaw", "Hufflepuff"],
+            cor: 1,
             img: "assets/images/snape.gif"
         },
         {
             q: "What was the first music video played on MTV?",
-            ch: ["Video Killed the Radio Star", "Mr. Roboto", "Whip It", "Safety Dance"],
-            co: "Video Killed the Radio Star",
+            cho: ["Video Killed the Radio Star", "Mr. Roboto", "Whip It", "Safety Dance"],
+            cor: 0,
             img: "assets/images/mtv.gif"
         },
         {
             q: "What song by Michael Jackson contains the lyrics “Annie are you OK?",
-            ch: ["Beat It", "P.Y.T.", "Smooth Criminal", "Thriller"],
-            co: "Smooth Criminal",
+            cho: ["Beat It", "P.Y.T.", "Smooth Criminal", "Thriller"],
+            cor: 2,
             img: "assets/images/michael.gif"
         },
         {
             q: "What does HTTP stand for in a website address?",
-            ch: ["Help Technology Teach People", "Here To Test People", "HyperText Transfer Protocol", "Hacking Tech True Purpose"],
-            co: "HyperText Transfer Protocol",
+            cho: ["Help Technology Teach People", "Here To Test People", "HyperText Transfer Protocol", "Hacking Tech True Purpose"],
+            cor: 2,
             img: "assets/images/webpage.gif"
         },
         {
             q: "In the original Gameboy game, which of these Pokemon was not a starter?",
-            ch: ["Bulbasaur", "Pikachu", "Charmander", "Squirtle"],
-            co: "Pikachu",
+            cho: ["Bulbasaur", "Pikachu", "Charmander", "Squirtle"],
+            cor: 1,
             img: "assets/images/pikachu.gif"
         },
         {
             q: "In the original Super Mario Bros. on NES, which item gives Mario special ability?",
-            ch: ["Green Mushroom", "Feather", "Red Mushroom", "Fire Flower"],
-            co: "Fire Flower",
+            cho: ["Green Mushroom", "Feather", "Red Mushroom", "Fire Flower"],
+            cor: 3,
             img: "assets/images/mario.gif"
         },
     ];
 
     // Set perimeters for start of game
-    var correctCount = 0;
+    var rightCount = 0;
     var wrongCount = 0;
-    var unansweredCount = 0;
-    var allCount = correctCount + wrongCount + unansweredCount;
+    var unansCount = 0;
+    // var allCount = rightCount + wrongCount + unansCount;
     var qCount = bank.length;
-    var userPicked = "";
-    var running = false;
+    var userPick = "";
+    var run = false;
     var timer = 15;
     var intervalId;
     var index;
-    var answer;
+    var stor;
     var newArr = [];
     var qHolder = [];
 
@@ -94,27 +94,27 @@ $(document).ready(function () {
 
     // Start the countdown timer
     function start() {
-        if (!running) {
+        if (!run) {
             intervalId = setInterval(decrement, 1000);
-            running = true;
+            run = true;
         }
     }
 
     // Stop the countdown timer
     function stop() {
         clearInterval(intervalId);
-        running = false;
+        run = false;
     }
 
     // Pull timer into the page
     function decrement() {
-        $('#timerDis').html("Time Remaining: " + timer + " seconds");
         timer--;
-        // Function to stop timer when it reaches 0
+        $('#timerDis').html("Time Remaining: " + timer + " seconds");
+        // Start of trivia; condition for out of time
         if (timer === 0) {
-            unansweredCount++;
             stop();
-            $('#answerDis').html("Took too long... Should of picked " + answer.choices[answer.correct]);
+            unansCount++;
+            $('#answerDis').html("Took too long... Should of picked " + stor.cho[stor.cor]);
             showGif();
         }
     }
@@ -122,36 +122,39 @@ $(document).ready(function () {
     // Picking a random question from array
     function displayQuest() {
         index = Math.floor(Math.random() * bank.length);
-        answer = bank[index];
+        stor = bank[index];
+        // console.log(stor);
 
         // Display the question and choices in window and loop through all
-        $('#questionDis').html(answer.q);
+        $('#questionDis').html(stor.q);
 
         // User must complete all questions to end
-        for (var i = 0; i < answer.choices.length; i++) {
-            let userChoice = $('<div>');
+        for (var i = 0; i < stor.cho.length; i++) {
+            let userChoice = $("<div>");
             userChoice.addClass("answerChoice");
-            userChoice.html(answer.choices[i]);
+            userChoice.html(stor.cho[i]);
             userChoice.attr("data-guessvalue", i);
             $('#answerDis').append(userChoice);
         }
 
         // Be able to click on answer followed by correct answer displayed
         $('.answerChoice').on('click', function () {
-            userPicked = $(this).attr("data-guessvalue");
+            userPick = parseInt($(this).attr("data-guessvalue"));
 
             // Guess outcomes of right or wrong
-            if (userPicked === answer.correct) {
+            if (userPick === stor.cor && timer > 0) {
                 stop();
-                correctCount++;
+                rightCount++;
                 userPicked = "";
-                $("#answerDis").html("You got THAT right!");
-                indexGif();
-            } else {
+                $("#answerDis").html("<p>You got THAT right!</p>");
+                showGif();
+                // Function to stop timer when it reaches 0
+            }
+            else {
                 stop();
                 wrongCount++;
                 userPicked = "";
-                $('#answerDis').html("Can't believe you didn't know that. Should of picked " + answer.choices[answer.correct]);
+                $('#answerDis').html("<p>Can't believe you didn't know that. Should of picked " + stor.cho[stor.cor] + "</p>");
                 showGif();
             }
         })
@@ -159,41 +162,42 @@ $(document).ready(function () {
 
     // Show GIF on answer screen
     function showGif() {
-        $('#answerDis').append("<img src=" + answer.img + ">");
-        newArr.push(answer);
+        $('#answerDis').append("<img src=" + stor.img + ">");
+        newArr.push(stor);
         bank.splice(index, 1);
 
-        let hidePic = setTimeout(function () {
+        let hidePic = setTimeout( function () {
             $('#answerDis').empty();
             timer = 15;
 
-            if (allCount === qCount) {
+            // Score shown after all questions have been answered
+            if ((rightCount + wrongCount + unansCount) === qCount) {
                 $("#questionDis").empty();
-                $("#questionDis").html("<h3>Game Over!  Here's how you did: </h3>");
-                $("#answerDis").append("<h4> Right: " + correctCount + "</h4>");
+                $("#questionDis").html("<h3>It's finally over!  Here's your tally count: </h3>");
+                $("#answerDis").append("<h4> Right: " + rightCount + "</h4>");
                 $("#answerDis").append("<h4> Wrong: " + wrongCount + "</h4>");
-                $("#answerDis").append("<h4> Out of Time: " + unanswerCount + "</h4>");
+                $("#answerDis").append("<h4> Out of Time: " + unansCount + "</h4>");
                 $("#resetBtn").show();
-                correctCount = 0;
+                rightCount = 0;
                 wrongCount = 0;
-                unanswerCount = 0;
+                unansCount = 0;
             } else {
-                runTimer();
+                start();
                 displayQuest();
 
             }
-        }, 3000);
+        }, 5000);
     }
 
     $("#resetBtn").on("click", function () {
         $("#resetBtn").hide();
         $("#answerDis").empty();
         $("#questionDis").empty();
-        for (var i = 0; i < holder.length; i++) {
-            bank.push(holder[i]);
+        for (var i = 0; i < qHolder.length; i++) {
+            bank.push(qHolder[i]);
         }
         runTimer();
-        displayQuestion();
+        displayQuest();
 
     })
 
